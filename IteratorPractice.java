b68de356ef74754dee2059894679a84cbb4adbab
jteresco <terescoj@gmail.com>
2022-02-13 15:22:58 -0500

    Fix indenting

--- IteratorPractice.java
+++ IteratorPractice.java
@@ -7,19 +7,18 @@ import java.util.Iterator;
  * In-class practice for working with Iterators.
  *
  * @author Jim Teresco, Completed by:
- * @version Spring 2021
+ * @version Spring 2022
  */
-public class IteratorPractice
-{
+public class IteratorPractice {
     public static void main(String args[]) {
-        
+
         // create a couple of lists that we can work with to practice with
         // our iterators
         List<Integer> a = new ArrayList<Integer>();
         for (int i = 1; i <= 20; i++) {
             a.add(i);
         }
-        
+
         List<String> b = new LinkedList<String>();
         b.add("my");
         b.add("favorite");
@@ -29,7 +28,7 @@ public class IteratorPractice
         b.add("is");
         b.add("probably");
         b.add("iterators");
-        
+
         // the built-in iterators (yes, we could also use an enhanced
         // for loop, but let's see the full Iterator functionality)
         System.out.println("a by the built-in iterator:");
@@ -38,12 +37,14 @@ public class IteratorPractice
             System.out.print(aIter.next() + " ");
         }
         System.out.println();
-    
+
         System.out.println("b by the built-in iterator:");
         Iterator<String> bIter = b.iterator();
         while (bIter.hasNext()) {
             System.out.print(bIter.next() + " ");
         }
         System.out.println();
+
+        // we will add test cases here for the new iterators
     }
 }
